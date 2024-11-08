59ab2df50d5578e8834e0337af89da357bbd1c07
LuisHorca <a01713551@tec.mx>
2024-11-07 20:30:07 -0600

    Delete Supplier.cpp

--- Supplier.cpp
+++
@@ -1,8 +0,0 @@
-#include "Supplier.h"
-
-Supplier::Supplier(const std::string &name, const std::string &contactInfo)
-    : name(name), contactInfo(contactInfo) {}
-
-std::string Supplier::getContactInfo() const {
-    return contactInfo;
-}
