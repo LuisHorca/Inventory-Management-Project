c62281802c92a54741fd96d70b8194e572604b15
LuisHorca <a01713551@tec.mx>
2024-11-07 21:02:09 -0600

    Product.cpp

--- Product.cpp
+++ Product.cpp
@@ -1,34 +1,35 @@
 #include "Product.h"
 
+Product::Product() 
+    : productID("1"), 
+      name("Crema"), 
+      price(15), 
+      quantity(5) {}
 
-Product::Product(
-    const std::string &id,
-    const std::string &name,
-    double price,
-    int quantity
-) : productID(id), name(name), price(price), quantity(quantity) {}
-
+Product::Product(const std::string& productID, const std::string& name, double price, int quantity)
+    : productID(productID), 
+      name(name), 
+      price(price), 
+      quantity(quantity) {}
 
 double Product::getPrice() const {
     return price;
 }
 
-
 int Product::getQuantity() const {
     return quantity;
 }
-
 
 void Product::setPrice(double newPrice) {
     price = newPrice;
 }
 
-
-void Product::addStock(int quantity) {
-    this->quantity += quantity;
+void Product::addStock(int qty) {
+    quantity += qty;
 }
 
-
-void Product::removeStock(int quantity) {
-    this->quantity -= quantity;
+void Product::removeStock(int qty) {
+    if (quantity >= qty) {
+        quantity -= qty;
+    }
 }
