e66bb170f24f1bc3384601f9a2f0a1fc72392bae
LuisHorca <a01713551@tec.mx>
2024-11-08 18:40:34 -0600

    Cans.cpp

--- Cans.cpp
+++ Cans.cpp
@@ -1,14 +1,11 @@
 #include "Cans.h"
 
-Cans::Cans() 
-    : Product(), 
-      contentVolume(0.0) {}
+Cans::Cans() {}
 
-Cans::Cans(const std::string& productID, const std::string& name, double price, int quantity, double contentVolume)
-    : Product(productID, name, price, quantity), 
-      contentVolume(contentVolume) {}
+Cans::Cans(std::string productID, std::string name, double price, int quantity, Supplier supplier, double contentVolume) {}
 
-double Cans::getContentVolume() const {
-    return contentVolume;
+double Cans::getContentVolume() {
+    return 0.0;
 }
+
 
