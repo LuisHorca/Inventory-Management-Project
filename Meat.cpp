3fbf860f1d7f463f6a0e1f56889d512cae74f75f
LuisHorca <a01713551@tec.mx>
2024-11-16 22:00:10 -0600

    Meat.cpp

--- Meat.cpp
+++ Meat.cpp
@@ -1,12 +1,20 @@
 #include "Meat.h"
 
-Meat::Meat(const std::string &productID, const std::string &name, double price, int quantity, const Supplier &supplier, const std::string &cutType, bool isFrozen)
+Meat::Meat(std::string productID, std::string name, double price, int quantity, Supplier supplier, std::string cutType, bool isFrozen)
     : Product(productID, name, price, quantity, supplier), cutType(cutType), isFrozen(isFrozen) {}
 
 std::string Meat::getCutType() const {
     return cutType;
 }
 
+void Meat::setCutType(std::string cutType) {
+    this->cutType = cutType;
+}
+
 bool Meat::getIsFrozen() const {
     return isFrozen;
+}
+
+void Meat::setIsFrozen(bool isFrozen) {
+    this->isFrozen = isFrozen;
 }
