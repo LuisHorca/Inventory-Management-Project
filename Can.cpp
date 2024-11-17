0a9ce9338137964d4e66b229949ffb251dd73d3a
LuisHorca <a01713551@tec.mx>
2024-11-16 21:58:40 -0600

    Can.cpp

--- Can.cpp
+++ Can.cpp
@@ -1,11 +1,16 @@
 #include "Can.h"
 
-Can::Can(const std::string &productID, const std::string &name, double price, int quantity, const Supplier &supplier, double contentVolume)
+Can::Can(std::string productID, std::string name, double price, int quantity, Supplier supplier, double contentVolume)
     : Product(productID, name, price, quantity, supplier), contentVolume(contentVolume) {}
 
 double Can::getContentVolume() const {
     return contentVolume;
 }
+
+void Can::setContentVolume(double contentVolume) {
+    this->contentVolume = contentVolume;
+}
+
 
 
 
