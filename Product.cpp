f1d89a282a7392bb43fc838dddf53e83b6a663eb
LuisHorca <a01713551@tec.mx>
2024-11-16 21:56:29 -0600

    Product.cpp

--- Product.cpp
+++ Product.cpp
@@ -1,18 +1,48 @@
 #include "Product.h"
 
-Product::Product(const std::string &productID, const std::string &name, double price, int quantity, const Supplier &supplier)
+Product::Product(std::string productID, std::string name, double price, int quantity, Supplier supplier)
     : productID(productID), name(name), price(price), quantity(quantity), supplier(supplier) {}
+
+Product::Product() : productID(""), name(""), price(0.0), quantity(0), supplier(Supplier()) {}
+
+std::string Product::getProductID() const {
+    return productID;
+}
+
+void Product::setProductID(std::string productID) {
+    this->productID = productID;
+}
+
+std::string Product::getName() const {
+    return name;
+}
+
+void Product::setName(std::string name) {
+    this->name = name;
+}
 
 double Product::getPrice() const {
     return price;
+}
+
+void Product::setPrice(double price) {
+    this->price = price;
 }
 
 int Product::getQuantity() const {
     return quantity;
 }
 
-void Product::setPrice(double newPrice) {
-    price = newPrice;
+void Product::setQuantity(int quantity) {
+    this->quantity = quantity;
+}
+
+Supplier Product::getSupplier() const {
+    return supplier;
+}
+
+void Product::setSupplier(Supplier supplier) {
+    this->supplier = supplier;
 }
 
 void Product::addStock(int quantity) {
@@ -20,11 +50,6 @@ void Product::addStock(int quantity) {
 }
 
 void Product::removeStock(int quantity) {
-    if (this->quantity >= quantity) {
-        this->quantity -= quantity;
-    }
+    this->quantity -= quantity;
 }
 
-Supplier Product::getSupplier() const {
-    return supplier;
-}
