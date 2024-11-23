fd3c0f5483244fa4956ecd183067df08714d7936
LuisHorca <a01713551@tec.mx>
2024-11-22 19:42:33 -0600

    Dairy.cpp

--- Dairy.cpp
+++ Dairy.cpp
@@ -1,21 +1,17 @@
 #include "Dairy.h"
 
+// Constructor: Initializes a dairy product with specific attributes.
 Dairy::Dairy(std::string productID, std::string name, double price, int quantity, Supplier supplier, bool isRefrigerated, double volume)
     : Product(productID, name, price, quantity, supplier), isRefrigerated(isRefrigerated), volume(volume) {}
 
-bool Dairy::getIsRefrigerated() const {
-    return isRefrigerated;
-}
+// Getter for refrigeration status.
+bool Dairy::getIsRefrigerated() const { return isRefrigerated; }
 
-void Dairy::setIsRefrigerated(bool isRefrigerated) {
-    this->isRefrigerated = isRefrigerated;
-}
+// Setter for refrigeration status.
+void Dairy::setIsRefrigerated(bool isRefrigerated) { this->isRefrigerated = isRefrigerated; }
 
-double Dairy::getVolume() const {
-    return volume;
-}
+// Getter for volume.
+double Dairy::getVolume() const { return volume; }
 
-void Dairy::setVolume(double volume) {
-    this->volume = volume;
-}
-
+// Setter for volume.
+void Dairy::setVolume(double volume) { this->volume = volume; }
