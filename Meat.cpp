09968492733bf7ce63ae403677a09c8f0bc02270
LuisHorca <a01713551@tec.mx>
2024-11-22 19:43:37 -0600

    Meat.cpp

--- Meat.cpp
+++ Meat.cpp
@@ -1,20 +1,17 @@
 #include "Meat.h"
 
+// Constructor: Initializes a meat product with specific attributes.
 Meat::Meat(std::string productID, std::string name, double price, int quantity, Supplier supplier, std::string cutType, bool isFrozen)
     : Product(productID, name, price, quantity, supplier), cutType(cutType), isFrozen(isFrozen) {}
 
-std::string Meat::getCutType() const {
-    return cutType;
-}
+// Getter for the type of cut.
+std::string Meat::getCutType() const { return cutType; }
 
-void Meat::setCutType(std::string cutType) {
-    this->cutType = cutType;
-}
+// Setter for the type of cut.
+void Meat::setCutType(std::string cutType) { this->cutType = cutType; }
 
-bool Meat::getIsFrozen() const {
-    return isFrozen;
-}
+// Getter for the frozen status.
+bool Meat::getIsFrozen() const { return isFrozen; }
 
-void Meat::setIsFrozen(bool isFrozen) {
-    this->isFrozen = isFrozen;
-}
+// Setter for the frozen status.
+void Meat::setIsFrozen(bool isFrozen) { this->isFrozen = isFrozen; }
