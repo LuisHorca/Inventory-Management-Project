3180a56cea86a397dedbc25ad485e807f600b6d1
LuisHorca <a01713551@tec.mx>
2024-11-22 19:41:44 -0600

    Can.h

--- Can.h
+++ Can.h
@@ -3,17 +3,20 @@
 
 #include "Product.h"
 
+// Class Can: Represents canned products with specific attributes.
 class Can : public Product {
 private:
-    double contentVolume;
+    double contentVolume; // Volume of the canned product in liters.
 
 public:
+    // Constructor to initialize a canned product.
     Can(std::string productID, std::string name, double price, int quantity, Supplier supplier, double contentVolume);
 
-    double getContentVolume() const;
-    void setContentVolume(double contentVolume);
+    // Getters and setters for contentVolume.
+    double getContentVolume() const; // Returns the content volume.
+    void setContentVolume(double contentVolume); // Sets the content volume.
 };
 
-#endif 
+#endif
 
 
