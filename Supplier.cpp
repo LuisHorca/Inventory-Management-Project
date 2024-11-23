ed64b5bdcea764f5460d0db22159e73ab5d93172
LuisHorca <a01713551@tec.mx>
2024-11-22 19:38:12 -0600

    Supplier.cpp

--- Supplier.cpp
+++ Supplier.cpp
@@ -1,22 +1,28 @@
 #include "Supplier.h"
 
+// Constructor: Initializes supplier with given name and contact information.
 Supplier::Supplier(std::string name, std::string contactInfo)
     : name(name), contactInfo(contactInfo) {}
 
+// Default constructor: Initializes an empty supplier.
 Supplier::Supplier() : name(""), contactInfo("") {}
 
+// Getter for the supplier's name.
 std::string Supplier::getName() const {
     return name;
 }
 
+// Setter for the supplier's name.
 void Supplier::setName(std::string name) {
     this->name = name;
 }
 
+// Getter for the supplier's contact information.
 std::string Supplier::getContactInfo() const {
     return contactInfo;
 }
 
+// Setter for the supplier's contact information.
 void Supplier::setContactInfo(std::string contactInfo) {
     this->contactInfo = contactInfo;
 }
